332e28b01a6d0e4dacfbbc9744165902bc13bafb
hjchoi <hjchoi@toss.im>
2024-02-21 00:25:09 +0900

    feat: 코틀린 코루틴 11장 - supervisorScope는 취소를 전파하지 않는다. -> 독립적인 작업에 사용

---
+++ kotlin-coroutine/src/main/kotlin/com/example/chapter11/SupervisorScopeExam.kt
@@ -0,0 +1,23 @@
+package com.example.chapter11
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.supervisorScope
+
+fun main() = runBlocking {
+    println("before")
+
+    supervisorScope {
+        launch {
+            delay(1000)
+            throw Error()
+        }
+
+        launch {
+            delay(2000)
+            println("done")
+        }
+    }
+    println("after")
+}

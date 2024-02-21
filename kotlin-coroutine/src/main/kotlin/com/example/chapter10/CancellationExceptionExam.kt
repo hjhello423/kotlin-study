0db3be2f1657b8fe35a45ba322808116fd6f9265
hjchoi <hjchoi@toss.im>
2024-02-21 10:42:54 +0900

    feat: 코틀린 코루틴 10장

---
+++ kotlin-coroutine/src/main/kotlin/com/example/chapter10/CancellationExceptionExam.kt
@@ -0,0 +1,22 @@
+package com.example.chapter10
+
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+object MyNonPropagatingException : CancellationException()
+
+suspend fun main(): Unit = coroutineScope {
+    launch {
+        launch {
+            delay(2000)
+            println("will not be printed")
+        }
+        throw MyNonPropagatingException
+    }
+    launch {
+        delay(2000)
+        println("will be printed")
+    }
+}
---
+++ kotlin-coroutine/src/main/kotlin/com/example/chapter10/SupervisorScopeExam.kt
@@ -0,0 +1,24 @@
+package com.example.chapter10
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.supervisorScope
+
+
+fun main(): Unit = runBlocking {
+    supervisorScope {
+        launch {
+            delay(1000)
+            throw Error("error")
+        }
+
+        launch {
+            delay(2000)
+            println("will be printed")
+        }
+    }
+
+    delay(1000)
+    println("done")
+}

a2d8aa9164afb615df562bdab38d21a43c4ba50b
hjhello423 <hjhello423@gmail.com>
2024-02-25 22:16:06 +0900

    feat: 코틀린 코루틴 12장 정리

---
+++ kotlin-coroutine/src/main/kotlin/com/example/chapter12/DispatcherIOExam.kt
@@ -0,0 +1,61 @@
+package com.example.chapter12
+
+import kotlinx.coroutines.*
+import java.util.concurrent.Executors
+import kotlin.system.measureTimeMillis
+
+suspend fun main_io1() = coroutineScope {
+    repeat(1000) {
+        launch(Dispatchers.IO) {
+            Thread.sleep(200)
+
+            val threadName = Thread.currentThread().name
+            println("running on thread: $threadName")
+        }
+    }
+}
+
+suspend fun main_io2(): Unit = coroutineScope {
+    launch(Dispatchers.Default) {
+        println(Thread.currentThread().name)
+        withContext(Dispatchers.IO) {
+            println(Thread.currentThread().name)
+        }
+    }
+}
+
+suspend fun main(): Unit = coroutineScope {
+    launch {
+        printCoroutinesTime(Dispatchers.IO)
+        // Dispatchers.IO took: 2074
+    }
+
+    launch {
+        val dispatcher = Dispatchers.IO
+            .limitedParallelism(100) // 100개로 설정
+        printCoroutinesTime(dispatcher)
+        // LimitedDispatcher@XXX took: 1082
+    }
+}
+
+suspend fun printCoroutinesTime(
+    dispatcher: CoroutineDispatcher
+) {
+    val test = measureTimeMillis {
+        coroutineScope {
+            repeat(100) {
+                launch(dispatcher) {
+                    Thread.sleep(1000)
+                }
+            }
+        }
+    }
+    println("$dispatcher took: $test")
+}
+
+suspend fun test(){
+    val NUMBER_OF_THREADS = 20
+    val dispatcher = Executors
+        .newFixedThreadPool(NUMBER_OF_THREADS)
+        .asCoroutineDispatcher()
+}

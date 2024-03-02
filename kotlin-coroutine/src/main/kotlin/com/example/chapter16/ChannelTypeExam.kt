7e4a91ae1e4ecf1302cab22be44497e84b139e33
hjhello423 <hjhello423@gmail.com>
2024-03-02 21:45:24 +0900

    feat: 코틀린 코루틴 16장 정리

--- kotlin-coroutine/src/main/kotlin/com/example/chapter16/ChannelTypeExam.kt
+++ kotlin-coroutine/src/main/kotlin/com/example/chapter16/ChannelTypeExam.kt
@@ -1,23 +1,60 @@
 package com.example.chapter16
 
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.channels.produce
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 
-suspend fun main(): Unit = coroutineScope {
+suspend fun main_Buffered(): Unit = coroutineScope {
     val channel = produce(capacity = 3) { // 버퍼 용량을 3으로 설정
         repeat(5) { index ->
-            val element = index * 2
-            send(element)
-            delay(100)
-            println("produce $element")
+            send(index)
+            println("produce: $index")
+            delay(1000)
         }
     }
 
-    delay(1000)
+    delay(5000)
+    println("---------------- after 5000 ----------------")
 
     for (element in channel) {
-        println(element)
+        println("received: $element")
+        delay(1000)
+    }
+}
+
+suspend fun main_rendezvous(): Unit = coroutineScope {
+    val channel = produce(capacity = Channel.RENDEZVOUS) { // 버퍼 용량을 0으로 설정 , default
+        repeat(5) { index ->
+            send(index)
+            println("produce: $index")
+            delay(1000)
+        }
+    }
+
+    delay(5000)
+    println("---------------- after 5000 ----------------")
+
+    for (element in channel) {
+        println("received: $element")
+        delay(1000)
+    }
+}
+
+suspend fun main(): Unit = coroutineScope {
+    val channel = produce(capacity = Channel.CONFLATED) { // 버퍼 용량을 1으로 설정
+        repeat(5) { index ->
+            send(index)
+            println("produce: $index")
+            delay(1000)
+        }
+    }
+
+    delay(4000)
+    println("---------------- after 4000 ----------------")
+
+    for (element in channel) {
+        println("received: $element")
         delay(1000)
     }
 }
---
+++ kotlin-coroutine/src/main/kotlin/com/example/chapter16/FanInExam.kt
@@ -0,0 +1,26 @@
+package com.example.chapter16
+
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.SendChannel
+
+suspend fun sendString(
+    channel: SendChannel<String>,
+    text: String,
+    time: Long
+) {
+    while (true) {
+        delay(time)
+        channel.send(text)
+    }
+}
+
+fun main() = runBlocking {
+    val channel = Channel<String>()
+    launch { sendString(channel, "foo", 200L) }
+    launch { sendString(channel, "BAR!", 500L) }
+    repeat(50) {
+        println(channel.receive())
+    }
+    coroutineContext.cancelChildren()
+}
